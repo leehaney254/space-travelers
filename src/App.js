7d942c61411290bef42320224b5036341ce0f5aa
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-30 21:54:14 +0300

    Feat: Add navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar/NavBar';
+import Rockets from './pages/Rockets';
+import Mission from './pages/Missions';
+import MyProfile from './pages/MyProfile';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Mission />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Rockets.js
@@ -0,0 +1,5 @@
+const Rockets = () => (
+  <h1>This is the Rockets.</h1>
+);
+
+export default Rockets;
