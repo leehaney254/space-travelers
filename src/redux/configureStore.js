964ad9d1c1d3eed56af6641501e0625d36ae9adf
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-31 16:03:24 +0300

    Feat: Add mission slice to store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import missionSlice from './missions/missions';
+
+const store = configureStore({
+  reducer: missionSlice,
+});
+
+export default store;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,1 +1,36 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  loading: false,
+  missions: [],
+  error: '',
+};
+
+const fetchMissions = createAsyncThunk('mission/fetchMissions', () => (
+  axios.get('https://api.spacexdata.com/v3/missions')
+    .then((res) => res.data)
+));
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.pending, (state) => (
+      { ...state, loading: true }
+    ));
+    builder.addCase(fetchMissions.fulfilled, (state, action) => (
+      {
+        ...state, isLoading: false, missions: action.payload, error: '',
+      }
+    ));
+    builder.addCase(fetchMissions.rejected, (state, action) => (
+      {
+        ...state, isLoading: false, missions: [], error: action.error.message,
+      }
+    ));
+  },
+});
+
+export default missionSlice;
+export { fetchMissions };
